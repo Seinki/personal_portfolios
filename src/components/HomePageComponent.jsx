4f17e0316bc9ee51425c66d819268aca94e757c9
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-12 18:11:24 +0700

    add & styling homepage component

---
+++ src/components/HomePageComponent.jsx
@@ -0,0 +1,27 @@
+import { Col, Container, Row } from "react-bootstrap";
+import personalData from "../data/personalData.json";
+
+export default function HomePageComponent() {
+  const heroImg = personalData.main.heroImg;
+  return (
+    <div className="home-page min-vh-100">
+      <Container>
+        <Row className="row-cols-2 d-flex justify-content-between align-items-center">
+          <Col>
+            <h3>Hi I&apos;m</h3>
+            <h1 className="fw-bold">Seinki Anna Lesmana</h1>
+            <h3 className="">Junior Web Developer</h3>
+            <div className="social-icons">
+              <i className="fa-brands fa-instagram"></i>
+              <i className="fa-brands fa-linkedin"></i>
+              <i className="fa-brands fa-github"></i>
+            </div>
+          </Col>
+          <Col>
+            <img src={heroImg} alt="hero-img" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
