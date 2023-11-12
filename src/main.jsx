9c9d20f5073527ef4a911aab19b4e89bd914fef3
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-12 16:36:11 +0700

    import main.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import '../src/data/css/main.css';
 {
   /* The following line can be included in your src/index.js or App.js file */
 }
