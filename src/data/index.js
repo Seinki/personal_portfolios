d0d8aeb7219f2800a13365be0d86e2034b240cb3
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-13 00:35:18 +0700

    add index.js for data

---
+++ src/data/index.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+  {
+    id: 1,
+    path: "",
+    text: "Home",
+  },
+  {
+    id: 2,
+    path: "about",
+    text: "About",
+  },
+  {
+    id: 3,
+    path: "exp",
+    text: "Experiences",
+  },
+  {
+    id: 4,
+    path: "portfolio",
+    text: "Portfolios",
+  },
+  {
+    id: 5,
+    path: "contact",
+    text: "Contact",
+  },
+]
