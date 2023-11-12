5e9863ebe64da13d23524af6b2bf78fed9c336c4
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-13 00:34:37 +0700

    update navlink using useState

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+import { navLinks } from "../data/index";
 
 export default function NavbarComponent() {
   const [changeColor, setChangeColor] = useState(false);
@@ -10,31 +13,41 @@ export default function NavbarComponent() {
     } else {
       setChangeColor(false);
     }
-  }
+  };
 
   useEffect(() => {
     changeBackgroundcolor();
-    window.addEventListener('scroll', changeBackgroundcolor);
+    window.addEventListener("scroll", changeBackgroundcolor);
   });
 
   return (
     <div className="navbar-page">
-      <Navbar fixed="top" expand="lg" className={`${changeColor ? "color-active" : ""} pb-2 pt-2 text-white shadow`}>
-      <Container>
-        <Navbar.Brand href="#home" className="fw-bold fs-3 text-white">SeinkiAL</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto text-white">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Resume</Nav.Link>
-            <Nav.Link href="#link">Experience</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <div className="dark-mode"></div>
-      </Container>
-    </Navbar>
+      <Navbar
+        fixed="top"
+        expand="lg"
+        className={`${
+          changeColor ? "color-active" : ""
+        } pb-2 pt-2 text-white shadow`}
+      >
+        <Container>
+          <Navbar.Brand href="#home" className="fw-bold fs-3 text-white">
+            SeinkiAL
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto text-white">
+              {navLinks.map((link) => {
+                return (
+                  <div className="nav-link" key={link.id}>
+                    <NavLink to={link.path}>{link.text}</NavLink>
+                  </div>
+                );
+              })}
+            </Nav>
+          </Navbar.Collapse>
+          <div className="dark-mode"></div>
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
