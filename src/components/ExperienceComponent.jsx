34f6c6a088230b4c7df8c3fdc3b4e641a9936dfa
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-13 13:06:59 +0700

    add exp component

---
+++ src/components/ExperienceComponent.jsx
@@ -0,0 +1,45 @@
+import { Accordion, Col, Container, Row } from "react-bootstrap";
+import { exp } from "../data/index";
+
+export default function ExperienceComponent() {
+  return (
+    <div className="exp-page min-vh-100 pt-5">
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="fw-bold text-center">My Experiences</h1>
+          </Col>
+        </Row>
+        <Row className="row-cols-1 pt-5">
+          <Col>
+            {exp.map((data) => {
+              return (
+                <div className="accordions" key={data.id}>
+                  <Accordion
+                    defaultActiveKey={data.defaultActiveKey}
+                    className="p-2"
+                  >
+                    <Accordion.Item eventKey={data.eventKey}>
+                      <Accordion.Header>{data.title}</Accordion.Header>
+                      <Accordion.Body>
+                        {data.jobDesc} | {data.time} <br />
+                        <hr />
+                        {data.desc.map((list) => {
+                          return (
+                            <ul key={list.id}>
+                              <li>{list.description}</li>
+                            </ul>
+                          )
+                        })}
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
