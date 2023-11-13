c86f7835b2c0c8b8f81d5eaa591fd3d5ad47b585
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-13 13:08:09 +0700

    update cards data

--- src/data/index.js
+++ src/data/index.js
@@ -46,3 +46,101 @@ export const cards = [
     desc: "Some quick example text to build on the card title and make up the bulk of the card%apos;s content.",
   },
 ];
+
+export const exp = [
+  {
+    id: 1,
+    defaultActiveKey: 0,
+    eventKey: 0,
+    title: "Ninja Xpress",
+    jobDesc: "Staff Warehouse",
+    time: "December 2021 - Present",
+    desc: [
+      {
+        id: 1,
+        description:
+          "Membantu mengangkat barang (paket) dalam karung maupun tidak ke dalam gudang.",
+      },
+      {
+        id: 2,
+        description:
+          "Membantu pemindaian awal paket menggunakan alat scanning barcode khusus resi untuk memilah dan memilih berdasarkan tujuan paket (kota / provinsi).",
+      },
+      {
+        id: 3,
+        description:
+          "Melakukan pemindaian menggunakan mesin DWS (Dimension, Weight, and Scanning) agar mendapatkan akurasi data paket yang baik dan benar berdasarkan dimensi, berat dan foto dari paket.",
+      },
+      {
+        id: 4,
+        description:
+          "Memindai paket dan memasukkannya ke dalam karung berlabel barcode khusus resi berdasarkan tujuan paket (kota).",
+      },
+    ],
+  },
+  {
+    id: 2,
+    defaultActiveKey: "",
+    eventKey: 1,
+    title: "Baraka Solution",
+    jobDesc: "Junior Web Programmer & IT Support",
+    time: "November 2022 - December 2022",
+    desc: [
+      {
+        id: 1,
+        description:
+          "Membantu membuat dan mengembangkan aplikasi web yang dibutuhkan Perusahaan.",
+      },
+      {
+        id: 2,
+        description:
+          "Membuat Prototype pengajuan desain website / web form dengan melakukan riset dan analisis kebutuhan pengguna dan mengumpulkan feedback dari pengguna, serta menerapkan keterampilan HTML, CSS, dan JavaScript untuk membuat prototype pengajuan desain website.",
+      },
+      {
+        id: 3,
+        description:
+          "Mengelola source code menggunakan tool GIT & GitHub agar dapat berkolaborasi antar tim dan mengelola repositori dengan baik dan benar.",
+      },
+      {
+        id: 4,
+        description:
+          "Melakukan uji coba untuk memastikan tampilan website berfungsi dengan baik.",
+      },
+      {
+        id: 5,
+        description:
+          "Mendaftarkan website ke layanan hosting dan melakukan restore data website jika terjadi masalah.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    defaultActiveKey: "",
+    eventKey: 2,
+    title: "Jati Kopi",
+    jobDesc: "Server/Waiter",
+    time: "Maret 2020 - November 2021",
+    desc: [
+      {
+        id: 1,
+        description:
+          "Memastikan pelanggan mendapatkan pelayanan yang ramah dan profesional selama kunjungan mereka di restoran.",
+      },
+      {
+        id: 2,
+        description:
+          "Bertanggung jawab untuk mencatat dengan akurat pesanan pelanggan dan mengkomunikasikannya ke dapur atau bar sesuai prosedur.",
+      },
+      {
+        id: 3,
+        description:
+          "Melibatkan tugas-tugas seperti membersihkan meja, menyediakan peralatan makan yang bersih, dan menjaga kebersihan area kerja untuk menciptakan lingkungan bersih dan nyaman.",
+      },
+      {
+        id: 4,
+        description:
+          "Mengelola transaksi pembayaran dengan akurat, memberikan tagihan kepada pelanggan, dan memberikan kembalian dengan tepat waktu.",
+      },
+    ],
+  },
+];
