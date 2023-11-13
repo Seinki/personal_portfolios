fff2c63b74aad6b06c33c9df637bf495987d54bf
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-13 09:44:50 +0700

    add cards data

--- src/data/index.js
+++ src/data/index.js
@@ -24,4 +24,25 @@ export const navLinks = [
     path: "contact",
     text: "Contact",
   },
-]
+];
+
+export const cards = [
+  {
+    id: 1,
+    title: "IT Support",
+    icon: "fa-solid fa-computer",
+    desc: "Some quick example text to build on the card title and make up the bulk of the card%apos;s content.",
+  },
+  {
+    id: 2,
+    title: "Web Development",
+    icon: "fa-solid fa-code",
+    desc: "Some quick example text to build on the card title and make up the bulk of the card%apos;s content.",
+  },
+  {
+    id: 3,
+    title: "Cyber Security",
+    icon: "fa-solid fa-shield-halved",
+    desc: "Some quick example text to build on the card title and make up the bulk of the card%apos;s content.",
+  },
+];
