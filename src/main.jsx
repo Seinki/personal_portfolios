cb4207f633ccb1b267fb4abc2e17e71993908d92
Seinki <121332397+Seinki@users.noreply.github.com>
2023-11-15 12:02:31 +0700

    import ScrollToTop

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,14 @@ import "../src/data/css/main.css";
   /* The following line can be included in your src/index.js or App.js file */
 }
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'animate.css';
+import "animate.css";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./pages/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
