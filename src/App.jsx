9665e910ba7ad447cc3ebf27915ea89d7db0743c
Seinki Anna Lesmana <seinkiart123@gmail.com>
2024-02-12 07:13:59 +0700

    remove ExperienceComponent route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import HomePageComponent from './components/HomePageComponent'
-import NavbarComponent from './components/NavbarComponent'
-import AboutComponent from './components/AboutComponent'
-import ExperienceComponent from './components/ExperienceComponent'
-import PortfolioComponent from './components/PortfolioComponent'
-import ContactComponent from './components/ContactComponent'
-import FooterComponent from './components/FooterComponent'
+import { Route, Routes } from "react-router-dom";
+import AboutComponent from "./components/AboutComponent";
+import ContactComponent from "./components/ContactComponent";
+import FooterComponent from "./components/FooterComponent";
+import HomePageComponent from "./components/HomePageComponent";
+import NavbarComponent from "./components/NavbarComponent";
+import PortfolioComponent from "./components/PortfolioComponent";
 
 function App() {
-
   return (
     <>
-      <NavbarComponent/>
+      <NavbarComponent />
       <Routes>
-        <Route path='/' Component={HomePageComponent}/>
-        <Route  path='/about' Component={AboutComponent}/>
-        <Route  path='/exp' Component={ExperienceComponent}/>
-        <Route  path='/portfolio' Component={PortfolioComponent}/>
-        <Route  path='/contact' Component={ContactComponent}/>
+        <Route path="/" Component={HomePageComponent} />
+        <Route path="/about" Component={AboutComponent} />
+        <Route path="/portfolio" Component={PortfolioComponent} />
+        <Route path="/contact" Component={ContactComponent} />
       </Routes>
       <FooterComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
