35920f1acf64cfd99aa922610236cfd993d12103
Seinki Anna Lesmana <seinkiart123@gmail.com>
2024-02-13 13:43:40 +0700

    update new desc story & add new button for read more-less

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -37,6 +37,7 @@ Content.propTypes = {
 export default function AboutComponent() {
   const [activeButton, setActiveButton] = useState("Specialized");
   const [isLoading, setLoading] = useState(false);
+  const [showMore, setShowMore] = useState(false);
 
   const handleClick = (button) => {
     setLoading(true);
@@ -44,6 +45,11 @@ export default function AboutComponent() {
     setTimeout(() => {
       setLoading(false);
     }, 800);
+  };
+
+  const handleClickReadMore = (e) => {
+    e.preventDefault();
+    setShowMore(true);
   };
 
   return (
@@ -71,40 +77,79 @@ export default function AboutComponent() {
               className="about-img"
             />
           </Col>
-          <Col className="col-12 col-lg-7 text-center">
+          <Col
+            className="col-12 col-lg-7 ps-5"
+            style={{ textAlign: "justify" }}
+          >
             <p data-aos="fade-up" data-aos-duration="1000">
-              I am a Warehouse Staff with over 2 years of experience in the
-              logistics industry. I have expertise in operating scanning tools
-              specialized in courier logistics. During my career, I have
-              successfully helped make BDO branches into branches with better
-              work performance results compared to other BDO branches.
+              As someone who has an educational background from Al Hadi Bandung
+              Vocational High School with a major in Network Computer
+              Engineering, I have had a diverse journey in the world of work and
+              learning. My first experience started as a waiter at Jati Kopi,
+              then switched to warehouse staff with DWS operator jobdesk. During
+              this time, I also had the opportunity to join an internship as a
+              Junior Web Programmer at Baraka Solution through the Work In Tech
+              Program.
             </p>
             <p data-aos="fade-up" data-aos-duration="1000">
-              I am very passionate and enthusiastic about deepening my knowledge
-              in IT, especially programming and cybersecurity. With my strong
-              background in Computer and Network Engineering, I am committed to
-              always improving my skills in this field to switch my career to
-              Web development. My main expertise involves computer operations,
-              both hardware and software. Not only that, but I am also quite
-              proficient in computer networking, web programming, and security
-              as evidenced by several related certifications on my LinkedIn
-              profile. By understanding the basics, we can achieve the desired
-              goal or result.
+              Participation in the Work In Tech Program with Yayasan Plan
+              International Indonesia through Coursera opened a new window for
+              me in developing IT skills. During this one-year program, I
+              managed to earn a Google Professional IT Support certificate and
+              attended an IT Bootcamp. This was the beginning of my journey in
+              collecting various IT-related certifications, including Frontend
+              and Backend web certificates from Dicoding, as well as Web
+              Development certificates from MySkill, Quality Assurance from
+              MySkill etc. I realize my lack of consistency, but I am constantly
+              striving to improve on that. In addition, I am also actively
+              involved in various small projects that include simple web
+              application development and technical troubleshooting. Some of
+              these projects can be found on my GitHub platform, which reflects
+              my abilities and interests in various aspects of software
+              development. Through these projects, I continue to hone my
+              programming skills and explore new concepts and technologies in
+              the IT industry.
             </p>
-            <p data-aos="fade-up" data-aos-duration="1000">
-              In addition, I am very enthusiastic about current technological
-              developments and I am always exploring. I love collaborating with
-              passionate people who have the vision to always be up-to-date in
-              technology. I am always open to new opportunities and
-              collaborations that can broaden my horizons and contributions. If
-              you are interested in connecting or discussing further, please
-              feel free to message me.
-            </p>
-            <p data-aos="fade-up" data-aos-duration="1000">
-              Thank you for your visit to my personal portfolios. I look forward
-              to contributing to achieving common goals and building meaningful
-              connections on LinkedIn.
-            </p>
+            {showMore && (
+              <>
+                <p data-aos="fade-up" data-aos-duration="1000">
+                  With diverse career interests ranging from being a Frontend,
+                  Backend, to Fullstack Web Developer, as well as interests in
+                  IT Support, Cyber Security, Pentester, and QA Engineer, I am
+                  ready to take on new challenges. I am open to internships,
+                  contracts, and remote or in-office work opportunities.
+                  Although my educational background is not a bachelor&apos;s
+                  degree, I believe that my dedication and hard work in
+                  developing my skills will be a plus for the company.
+                </p>
+                <p data-aos="fade-up" data-aos-duration="1000">
+                  I am grateful for every opportunity that has been given to me
+                  and I sincerely hope to be part of a dynamic and innovative
+                  team, where I can contribute significantly and continue to
+                  learn and grow together. Thank you for your attention and I
+                  sincerely hope to contribute as a competent and dedicated
+                  candidate.
+                </p>
+                <button
+                  data-aos="fade-up"
+                  data-aos-duration="1000"
+                  className="btn_readMoreLess"
+                  onClick={() => setShowMore(false)}
+                >
+                  Read Less
+                </button>
+              </>
+            )}
+            {!showMore && (
+              <button
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                className="btn_readMoreLess"
+                onClick={handleClickReadMore}
+              >
+                Read More
+              </button>
+            )}
           </Col>
         </Row>
 
