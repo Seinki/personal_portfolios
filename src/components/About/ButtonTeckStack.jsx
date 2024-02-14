fc42e71061b4b2e6c927d4442c80010f2f68f2c0
Seinki Anna Lesmana <seinkiart123@gmail.com>
2024-02-15 04:00:26 +0700

    update layout style

--- src/components/About/ButtonTeckStack.jsx
+++ src/components/About/ButtonTeckStack.jsx
@@ -10,7 +10,7 @@ const ButtonTechStack = ({ title, onClick, isActive }) => {
   return (
     <Col>
       <div className={isActive ? "actived" : "inactived"} onClick={handleClick}>
-        <button className="btn_techStack px-2 py-2 px-lg-4 py-lg-3 fs-6">
+        <button className="btn_techStack px-3 py-2 px-lg-4 py-lg-3 fs-6">
           {title}
         </button>
       </div>
